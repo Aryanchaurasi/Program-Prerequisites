bfd9a014ac3b04dd3e7180ad087bdc7e0334a65b
ARYANCHAURASIA <aryanchaurasi20@gmail.com>
2025-01-08 10:38:21 +0530

    The program of converison of Km to mile added

---
+++ Km.java
@@ -0,0 +1,11 @@
+import java.util. Scanner;
+public class Km
+{
+	public static void main(String[] args) {
+	    
+		Scanner sc= new Scanner(System.in);
+		double Kilometers  = sc.nextInt();
+		double Miles = Kilometers * 0.621371;
+		System.out.println("The Converted value is   "+ Miles);
+	}
+}
